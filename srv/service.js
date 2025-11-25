b9b8d8ec4004ea13dee3d8c4930a412f9c12997d
RJTechRamjee <koradaramjee@outlook.com>
2025-11-25 12:32:45 +0100

    ajused

--- srv/service.js
+++ srv/service.js
@@ -1,23 +1,12 @@
 const cds = require('@sap/cds')
 const { header } = require('@sap/cds/lib/i18n/locale')
 module.exports = async (srv) => {
-    const travel_metadata = await cds.connect.to('travel_metadata')
+    const externalService = await cds.connect.to('travel_metadata')
     const { Travel } = srv.entities
 
     srv.on(['READ'], Travel, async (req) => {
         let TravelRequestsQuery = SELECT.from(req.query.SELECT.from).limit(req.query.SELECT.limit);
-        if (req.query.SELECT.where) {
-            TravelRequestsQuery.where(req.query.SELECT.where)
-        }
-
-        if (req.query.SELECT.orderBy) {
-            TravelRequestsQuery.orderBy(req.query.SELECT.orderBy)
-        }
-
-              let TravelRequestsResult = await travel_metadata.tx(req).send({
-            query: TravelRequestsQuery
-         
-        })
+        let TravelRequestsResult = await externalService.run(TravelRequestsQuery);
         return TravelRequestsResult;
 
     }
